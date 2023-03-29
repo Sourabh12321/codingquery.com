69f27f95af83b86d557f17f89694b2b40f15d2b4
unknown <sourabh.rajput.22082001@gmail.com>
2023-03-29 22:37:54 +0530

    day-2

---
+++ backend/utils/users.js
@@ -0,0 +1,31 @@
+const users = [];
+
+function userjoin(id,username,room){
+    const user = {id,username,room};
+    users.push(user);
+
+    return user;
+
+}
+
+function getcurrentuser(id){
+    return users.find(user=>user.id ===id);
+}
+
+function userleave(id){
+    const index = users.findIndex(user=>user.id===id);
+    if(index!==-1){
+        return users.splice(index,1)[0];
+    }
+}
+
+function getroomusers(room){
+    return users.filter(user=>user.room===room);
+}
+
+module.exports = {
+    userjoin,
+    getcurrentuser,
+    userleave,
+    getroomusers
+}
